393786cb9db3d26b4866e8c3975b47fefdf7a541
Caroline Karanja <ckaranja24@gmail.com>
2016-01-08 14:57:25 -0600

    added devise and migration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #required for heroku. use the real host name.
+  config.action_mailer.default_url_options = { host: 'http://ovawomanapp.herokuapp.com/' }
 end
